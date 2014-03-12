26aeb473471601cbcb40a75722c232affa667a17
Maya Saxena <maya.saxena@tufts.edu>
2014-03-11 23:36:58 -0400

    trying to display map

--- a3/rodeo.js
+++ a3/rodeo.js
@@ -1,18 +1,55 @@
-var xhr;
-function init() {
-        xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
+var request;
+var mbta;
+var myLat;
+var myLong;
+var myLoc = new google.maps.LatLng(myLat, myLng);
+var myOptions = {
+                        zoom: 13, // The larger the zoom number, the bigger the zoom
+                        center: myLoc,
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+var map;
+var marker;
+var infowindow = new google.maps.InfoWindow();
+var line;
+var stations;
 
-        xhr.onreadystatechange = dataReady; //rval has to be a function
-        xhr.send(null);
+
+function init() {
+        mbta = new XMLHttpRequest();
+        mbta.open("GET", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
+        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+        getMyLocation();
+
+
 }
 
-function dataReady() {
-
-        if(xhr.readyState  == 4) {
-                schedule = JSON.parse(xhr.responseText);
-                console.log(schedule);
-                scheduleDOM = document.getElementById("schedule");
-                scheduleDOM.innerHTML = schedule["line"];
+function getMyLocation()
+{
+        if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+                navigator.geolocation.getCurrentPosition(function(position) {
+                        myLat = position.coords.latitude;
+                        myLong = position.coords.longitude;
+                        renderMap();
+                });
         }
+        else {
+                alert("Geolocation is not supported by your web browser.");
+        }
+}
+
+function renderMap()
+{
+        myLoc = new google.maps.LatLng(myLat, myLong);
+        
+        // Update map and go there...
+        map.panTo(myLoc);
+
+        // Create a marker
+        marker = new google.maps.Marker({
+                position: myLoc,
+                title: "Current Location"
+        });
+        marker.setMap(map);
+                
 }
