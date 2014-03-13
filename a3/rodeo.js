5fbab8c171efd51c6249a0cdf222660ed098a9f9
Maya Saxena <maya.saxena@tufts.edu>
2014-03-13 15:23:29 -0400

    reading stations pt5

--- a3/rodeo.js
+++ a3/rodeo.js
@@ -38,6 +38,7 @@ function readTextFile(file)
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
+                console.log(allText);
             }
         }
     }
