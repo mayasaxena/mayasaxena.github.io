aec6e5d97989497b236d24e4e5c59b6f696268bd
Maya Saxena <maya.saxena@tufts.edu>
2014-03-13 16:00:58 -0400

    reading stations pt18

--- a3/rodeo.js
+++ a3/rodeo.js
@@ -29,8 +29,7 @@ function init() {
 
 function readCSV()
 {
-    stationstr = "Blue,Airport,42.374262,-71.030395
-                    Blue,Aquarium,42.359784,-71.051652";
+    stationstr = "Blue,Airport,42.374262,-71.030395";
     console.log(stationstr);
 }
 
