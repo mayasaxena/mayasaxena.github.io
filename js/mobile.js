a16d360ab5fda48fce533ced853d310096af61d2
Maya Saxena <maya.saxena@tufts.edu>
2014-10-15 22:17:23 -0400

    refixed click slide

--- js/mobile.js
+++ js/mobile.js
@@ -14,14 +14,4 @@ $(".navicon").click(function() {
 });
 
 
-$(document).click(function(event) { 
-    if(!$(event.target).is('.navicon')) {
-        if($("nav ul").is(":visible") && $(window).width() < 800) {
-            $("nav ul").slideUp();
-        }
-    }        
-});
-
-
-
 });
