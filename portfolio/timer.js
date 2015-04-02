c51cc8a2d73480a71e0051ded24519022a0d890a
Maya Saxena <maya.saxena@tufts.edu>
2015-04-01 21:27:57 -0400

    fixed

--- portfolio/timer.js
+++ portfolio/timer.js
@@ -52,7 +52,7 @@ function checkAnswers(correctAnswers) {
     if (localStorage.page == 1) {
         values = $('#items').val();
     } else if (localStorage.page == 2) {
-        values = $('#items input:checked');
+        values = $('#items input:checked').val();
     }
     console.log(values);
 }
