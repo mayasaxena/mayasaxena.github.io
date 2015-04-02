5a06c4838705d947e7f20ed30794368fa5acb689
Maya Saxena <maya.saxena@tufts.edu>
2015-04-01 22:58:03 -0400

    fixed

--- portfolio/timer.js
+++ portfolio/timer.js
@@ -60,7 +60,7 @@ function checkAnswers(correctAnswers) {
 
     console.log(values);
     console.log(correctAnswers);
-    if(values.equals(correctAnswers) {
+    if(values.equals(correctAnswers)) {
         console.log("matched");
     }
 
